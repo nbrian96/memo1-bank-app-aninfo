f82e18848818fd4a5c5673e6d94258411d5eb1a2
Nicolas Outeda <nicolas.outeda@glovoapp.com>
2021-05-10 22:55:41 +0200

    cross origin

--- src/main/java/com/aninfo/DemoApplication.java
+++ src/main/java/com/aninfo/DemoApplication.java
@@ -20,6 +20,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @SpringBootApplication
 @EnableSwagger2
